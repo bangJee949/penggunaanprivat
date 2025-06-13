4a2e9cbe54fccb40db7f949a1551e07c8e375ccc
bangJee949 <83695284+bangJee949@users.noreply.github.com>
2025-06-13 14:32:29 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function showTab(index) {
-  document.querySelectorAll('.tab').forEach((tab, i) => {
-    tab.classList.toggle('active', i === index);
-    document.getElementById(`tab-${i}`).classList.toggle('hidden', i !== index);
-  });
-}
